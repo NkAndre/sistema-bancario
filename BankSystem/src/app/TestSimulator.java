48b0931a3f9fa9dd2c375afd4072312169bc73c8
NkAndre <andre270708@gmail.com>
2026-01-19 19:10:18 -0300

    initial commit

---
+++ BankSystem/src/app/TestSimulator.java
@@ -0,0 +1,31 @@
+package app;
+
+import model.Cliente;
+import service.BancoService;
+
+// esta classe simulada dados e operações automaticamente 
+
+public class TestSimulator {
+
+	/*public static void simularOperacoes(BancoService banco) {
+         Cliente c1 = new Cliente("André", 123);
+        Cliente c2 = new Cliente("Maria", 456);
+
+        banco.criarCliente(c1);
+        banco.criarCliente(c2);
+
+        banco.depositar(123, 500);
+        banco.sacar(123, 200);
+        banco.consultarSaldo(123);
+
+        banco.depositar(456, 1000);
+        banco.consultarSaldo(456);
+
+        banco.transferir(456, 123, 300);
+        banco.consultarSaldo(123);
+        banco.consultarSaldo(456);
+
+        banco.mostrarHistorico(123);
+        banco.mostrarHistorico(456);
+    } */
+} 
